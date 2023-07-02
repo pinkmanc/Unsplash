071b6ff469dd0812c4064ebd1c80c2d677a794a3
pinkmanc <2602616997@qq.com>
2023-07-02 13:16:50 +0800

    develop

--- app/src/main/java/com/example/drysister/ui/activity/MainActivity.java
+++ app/src/main/java/com/example/drysister/ui/activity/MainActivity.java
@@ -3,22 +3,45 @@ package com.example.drysister.ui.activity;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
 
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 import com.example.drysister.R;
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private Toolbar toolbar;
+    private DrawerLayout drawer_layout;
+    private NavigationView nav_view;
+    private TextView tv_nav_title;
+    private ConstraintLayout drawer_layout_main_content;
+
+    private FragmentManager mFgManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
 
     }
+    protected void initView(){
+        toolbar = findViewById(R.id.toolbar);
+        nav_view = findViewById(R.id.drawer_layout);
+        tv_nav_title = nav_view.getHeaderView(0).findViewById(R.id.tv_nav_title);
+        drawer_layout = findViewById(R.id.drawer_layout);
+        drawer_layout_main_content = findViewById(R.id.drawer_layout_main_content);
+
+
+    }
+
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
