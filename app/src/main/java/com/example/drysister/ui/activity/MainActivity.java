b5809e1479d5158422b36f223680868975f5a0f9
pinkmanc <2602616997@qq.com>
2023-07-02 12:11:42 +0800

    初次提交

---
+++ app/src/main/java/com/example/drysister/ui/activity/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.drysister.ui.activity;
+
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+
+import com.example.drysister.R;
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.nav_see_little_sister:
+
+        }
+
+        return true;
+    }
+}
---
+++ app/src/main/java/com/example/drysister/ui/activity/SplashActivity.java
@@ -0,0 +1,38 @@
+package com.example.drysister.ui.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.drysister.R;
+
+/**
+ * 描述:splash闪屏页
+ *
+ * @author pinkmanc
+ */
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        TextView tv_logo = findViewById(R.id.tv_logo);
+        tv_logo.postDelayed(this::jump,2000L);
+
+    }
+
+    /* 完成一些初始化操作 */
+    private void init() {
+
+    }
+
+    /* 页面逻辑跳转 */
+    private void jump() {
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+
+}
